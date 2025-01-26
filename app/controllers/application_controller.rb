33ceef4c5025c9dfce755f7e5342b173687406f9
Oskar Puzio <osk.vitae@gmail.com>
2025-01-26 05:03:43 +0000

    username sign up and edit feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # Is this the best way to add usrname?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Validations
+  validates :username, presence: true
+  validates :username, uniqueness: true
+
+  has_many :posts
+  has_many :event_attendances
+  has_many :events, through: :event_attendances
+  has_many :follower_relationships, class_name: "Follow", foreign_key: "source_id"
+  has_many :follower_relationships, class_name: "Follow", foreign_key: "target_id"
 end
